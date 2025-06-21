9bc9ae5af699e0c1b5530abdd86bcb220fa27e22
Amin71518 <gormarg2004@gmail.com>
2025-06-21 23:20:45 +0400

    Задание 2.1

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/public/task2.1/index.html`);
+  res.sendFile(`${__dirname}/public/index.html`);
 });
 
 
 app.listen(3333, () => {
-    console.log('Application listening on port 3333');
+    console.log('Application listening on port 3333 (task2.1)');
 });
