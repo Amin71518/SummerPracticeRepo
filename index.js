08932f5f149bf91970cdf06b3c8aeaca10ac7831
Amin71518 <gormarg2004@gmail.com>
2025-06-22 16:17:23 +0400

    добавил css к task2.1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/public/task2.1/index.html`);
+  res.sendFile(`${__dirname}/public/index.html`);
 });
 
 
